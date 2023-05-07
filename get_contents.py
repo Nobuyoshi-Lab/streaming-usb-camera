2450cc7dba3e6147c8f46be76f866ab9f4a6c5ed
Ka Ming Lui <s07091012@gmail.com>
2023-05-07 05:59:49 -0400

    idk

--- get_contents.py
+++ get_contents.py
@@ -6,16 +6,19 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
+# Get script directory path
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
 # Open file dialog and get selected file paths
 file_paths = filedialog.askopenfilenames()
 
 # Loop through selected files and append their content to new content
 new_content = ""
 for path in file_paths:
-    # Get file name
-    file_name = os.path.basename(path)
+    # Get relative file path
+    rel_path = os.path.relpath(path, script_dir)
     # Add colon to file name
-    file_name += ":"
+    file_name = rel_path + ":"
     # Read file content
     with open(path, "r") as file:
         content = file.read()
@@ -23,10 +26,14 @@ for path in file_paths:
     new_content += f"{file_name}\n{content}\n\n"
 
 # Set output file path to "output.txt" in the same directory as the script
-output_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output.txt")
+output_file_path = os.path.join(script_dir, "output.txt")
+
+# Clear output file contents
+with open(output_file_path, "w") as output_file:
+    pass
 
 # Write new content to file
-with open(output_file_path, "w") as output_file:
+with open(output_file_path, "a") as output_file:
     output_file.write(new_content)
 
 print("Files appended and saved to the output file.")
