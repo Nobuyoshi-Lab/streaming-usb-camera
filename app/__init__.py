061f001e0bbcb54d517be150146eb9f6f7d0410f
Ka Ming Lui <tuk91907@temple.edu>
2023-05-07 03:22:29 -0400

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
-import io
 import cv2
 import os
-import numpy as np
-from flask import Flask, Response, send_from_directory, request
+from flask import Flask, Response, send_from_directory
 
 app = Flask(__name__, static_folder=None)
 react_build_folder = 'frontend/build'
@@ -20,7 +18,8 @@ def camera_feed():
     return Response(generate_camera_feed(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def generate_camera_feed():
-    cap = cv2.VideoCapture(0)
+    video_device = os.environ.get('VIDEO_DEVICE', 0)
+    cap = cv2.VideoCapture(int(video_device) if str(video_device).isdigit() else video_device)
     while True:
         ret, frame = cap.read()
         ret, jpeg = cv2.imencode('.jpg', frame)
--- run_camera_web.py
+++ run_camera_web.py
@@ -1,14 +1,12 @@
-import os
 import platform
 import subprocess
 import sys
+import cv2
 
 def main():
     system = platform.system()
 
     if system == "Linux":
-        subprocess.run(["sudo", "usermod", "-aG", "video", os.getlogin()])
-
         v4l2_output = subprocess.check_output(["v4l2-ctl", "--list-devices"]).decode("utf-8")
         camera_device = v4l2_output.split("(\/dev\/video")[1].split(")")[0]
 
@@ -16,28 +14,30 @@ def main():
             print("No camera device found.")
             sys.exit(1)
 
-        os.environ["VIDEO_DEVICE"] = f"/dev/video{camera_device}"
+        video_device = f"/dev/video{camera_device}"
     elif system == "Windows":
-        try:
-            import pydirectshow
-        except ImportError:
-            subprocess.run([sys.executable, "-m", "pip", "install", "pydirectshow"])
+        camera_device = None
+        for i in range(10):
+            cap = cv2.VideoCapture(i)
+            if cap is None or not cap.isOpened():
+                continue
+            camera_device = i
+            cap.release()
+            break
 
-        import pydirectshow
-        camera_device = pydirectshow.enumerate_devices()[0][0]
-
-        if not camera_device:
+        if camera_device is None:
             print("No camera device found.")
             sys.exit(1)
 
-        os.environ["VIDEO_DEVICE"] = f"\\\\.\\pipe\\video{camera_device}"
+        video_device = str(camera_device)
     else:
         print("Unsupported OS.")
         sys.exit(1)
 
-    print(f"Using camera device: {os.environ['VIDEO_DEVICE']}")
+    print(f"Using camera device: {video_device}")
 
-    subprocess.run(["docker-compose", "up"])
+    with open(".env", "w") as env_file:
+        env_file.write(f"VIDEO_DEVICE={video_device}\n")
 
 if __name__ == "__main__":
     main()
